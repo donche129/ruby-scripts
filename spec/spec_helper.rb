e93135b10eb0bc22ef8ee26655cd8b79816538a2
Donald Cherestal <donaldcherestal@gmail.com>
2021-09-09 15:30:49 -0400

    requirements and screenshots

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,14 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'webdrivers'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'fileutils'
+require 'logger'
+require 'helper_functions'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -43,6 +51,14 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # Screenshots
+  config.after(:each) do |example|
+    if example.exception
+      page.save_page
+      page.save_screenshot
+    end
+  end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
